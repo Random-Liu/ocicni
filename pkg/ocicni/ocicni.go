78d05075bdd0789cc2f465eb160e66c7902b0b60
Lantao Liu <lantaol@google.com>
2018-02-05 22:11:34 +0000

    Ensure cni config directory exists.
    Signed-off-by: Lantao Liu <lantaol@google.com>

--- pkg/ocicni/ocicni.go
+++ pkg/ocicni/ocicni.go
@@ -173,9 +173,9 @@ func InitCNI(pluginDir string, cniDirs ...string) (CNIPlugin, error) {
 		return nil, err
 	}
 
-	// Fail loudly if plugin directory doesn't exist, because fsnotify watcher
-	// won't be able to watch it.
-	if _, err := os.Stat(pluginDir); err != nil {
+	// Ensure plugin directory exist, because following monitoring logic
+	// relies on that.
+	if err := os.MkdirAll(pluginDir); err != nil {
 		return nil, err
 	}
 
